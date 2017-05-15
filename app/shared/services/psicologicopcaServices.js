c665ff1070f2ed1bc52723efb37c1899629c82c4
danielgonzalor <danielgonzalor@gmail.com>
2017-05-15 13:37:22 -0400

    Psicologico,
    implementacion de guardado identity en local storage, se rescata url
    desde el servidor soap, se verifica si los datos existen en local se
    muestra evaluacion y si no crea una peticion a soap para mostrar una
    nueva evaluacion

--- app/shared/services/psicologicopcaServices.js
+++ app/shared/services/psicologicopcaServices.js
@@ -1,10 +1,19 @@
 angular.module('ApiRest')
 .factory('psicologicopcaServices', ['WebApiConfig','$http','$httpParamSerializer','sessionServices', function(WebApiConfig,$http,$httpParamSerializer,sessionServices){
 	return { 
-		send: function(data){
+		inscripcion: function(data){
                     return $http.post(WebApiConfig.resourceUrl('psicologicopca'),data);
                   }
         ,
+        gotosurvey: function(data){
+                    return $http.post(WebApiConfig.resourceUrl('psicologicopca/gotosurvey'),data);
+                  }
+		,
+        getresult : function(data){
+                    return $http.post(WebApiConfig.resourceUrl('psicologicopca/getresult'),data);
+                  }
+        ,
+
 		credential:function() {
 				if(!sessionServices.expire){$http.defaults.headers.common.Authorization="Bearer "+sessionServices.token;}else{delete $http.defaults.headers.common.Authorization;}
 		}
