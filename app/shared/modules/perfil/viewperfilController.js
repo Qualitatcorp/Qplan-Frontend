7e0faf750239b075ff8f3c0e70f6d73c28c36631
Ruben <rbntejeda@hotmail.com>
2017-05-29 11:01:45 -0400

    Save

--- app/shared/modules/perfil/viewperfilController.js
+++ app/shared/modules/perfil/viewperfilController.js
@@ -1,9 +1,20 @@
 angular.module('Perfil')
 
-.controller('perfil.viewperfilController', ['toastr','apiServices','$scope','perfil','$location', function(toastr,apiServices,$scope,perfil,$location){
+.controller('perfil.viewperfilController', ['toastr','apiServices','$scope','perfil','$location','$http','$window','FileSaver', function(toastr,apiServices,$scope,perfil,$location,$http,$window,FileSaver){
+	
 	$scope.perfil=perfil.data;
-
-		$scope.remove=function(id){
+	$scope.pdf;
+	$http.get("http://localhost/Qplan-Frontend/Doc/asdPractica.pdf",{responseType: 'arraybuffer'}).then(function(q) {
+		var file = new Blob([q.data],{type:'application/pdf'});
+		// console.log(file);
+		var fileURL = URL.createObjectURL(file);
+		$scope.pdf=fileURL;
+		// console.log(fileURL);
+		// console.log(q);
+		// $window.open(fileURL);
+		// FileSaver.saveAs(file, 'ficha.pdf');
+	})
+	$scope.remove=function(id){
 		apiServices.model('perfil').remove(id).then(function(q) {
 			toastr.success("Se ha eliminado con exito.","Exito");
 			$location.path("perfil/admin");
